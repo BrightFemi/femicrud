9dec87a07ec730f684b1d672e4b0b106280cf8ed
BrightFemi <105293456+BrightFemi@users.noreply.github.com>
2022-06-22 14:47:04 +0100

    Add files via upload

---
+++ brightproj/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path,include
+
+app_name = 'blog'
+
+urlpatterns = [  
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls', namespace='blog'))
+]
+
+
+
